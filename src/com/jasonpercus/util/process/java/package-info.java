eaff685a061ab3edb36cd69b7baa5b267192c61f
Joseph Briguet <jbriguet@s-id.fr>
2021-05-19 10:31:21 +0200

    :heavy_plus_sign: v1.1 - Ajout du projet InterPipe

---
+++ src/com/jasonpercus/util/process/java/package-info.java
@@ -0,0 +1,4 @@
+/**
+Ce package contient quelques classes permettant de lancer un processus java à partir d'un *.jar et de pouvoir communiquer de manière bidirectionnelle avec lui
+*/
+package com.jasonpercus.util.process.java;
