69ebffa7666883ba16594ae2bd69a54d0b2c88d3
Joseph Briguet <jbriguet@s-id.fr>
2021-05-28 16:41:14 +0200

    :construction: v1.2 - Ajout de la classe Zone (MouseArea)

---
+++ src/com/jasonpercus/util/mousearea/package-info.java
@@ -0,0 +1,4 @@
+/**
+Ce package contient toutes les classes permettant à un système utilisant un pointer de détecter une zone précise d'un {@link java.awt.Component}
+*/
+package com.jasonpercus.util.mousearea;
